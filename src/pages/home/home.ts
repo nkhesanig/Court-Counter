4648c6707cf1db8181bc5b61407295c1b5246066
nkhesani <nkhesani.mukoveni@gmail.com>
2018-06-26 14:04:19 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  teamA=0;
+  teamB= 0;
+
   constructor(public navCtrl: NavController) {
 
+    this.teamA=0;
+    this.teamB=0;
+
   }
+
+  onclickk(num:number){
+    if(num==3){
+      this.teamA +=3;
+    }
+    else if(num==2){
+      this.teamA +=2;
+    }
+    else {
+      this.teamA +=5;
+    }
+
+
+
+
+  }
+
+  onclick(num:number){
+    if(num==3){
+      this.teamB +=3;
+    }
+    else if(num==2){
+      this.teamB +=2;
+    }
+    else {
+      this.teamB +=5;
+    }
+
+
+
+
+  }
+
+
+click(){
+  this.teamA =0;
+  this.teamB =0;
+}
 
 }
